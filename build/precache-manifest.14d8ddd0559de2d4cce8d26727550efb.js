66523a84bf44315e7dbbb680574c4c8f03ff2f77
asuperhero <42861396+asuperhero@users.noreply.github.com>
2018-12-07 14:39:16 +0800

    react

---
+++ build/precache-manifest.14d8ddd0559de2d4cce8d26727550efb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "873fc02d326f933370bd",
+    "url": "/static/js/main.873fc02d.chunk.js"
+  },
+  {
+    "revision": "75af277f8bc13d38c0b6",
+    "url": "/static/js/1.75af277f.chunk.js"
+  },
+  {
+    "revision": "873fc02d326f933370bd",
+    "url": "/static/css/main.f1aaed31.chunk.css"
+  },
+  {
+    "revision": "14cd5e0688d44fc4443dec5caa32577c",
+    "url": "/index.html"
+  }
+];
